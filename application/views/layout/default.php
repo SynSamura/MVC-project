961eabc7edb0de422c1e8a1b67142e867a2f5554
Evgeniy Lozhkov <ew1enii2575@mail.ru>
2023-10-05 18:13:07 +0300

    Правки

--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
+    <script src = "/public/scripts/jquery.js"></script>
+    <script src = "/public/scripts/form.js"></script>
 </head>
 <body>
+
+
+
 <?php echo $content; ?>
 </body>
 </html>
